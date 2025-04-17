544c3b1d4747f8de075494e111869759846b05a6
Prachi-kumari-1111 <prachikri8840@gmail.com>
2025-04-17 10:06:41 +0530

    e:\JavaScript\Javascript\Lecture 7\three.js

---
+++ Lecture 7/three.js
@@ -0,0 +1,8 @@
+function EvaluateExpression() {
+ const result = 5 + 3 * (2 ** 2) / 4 - 1;
+    return result;
+}
+
+
+const value = EvaluateExpression();
+console.log("Value of the expression:", value); 
