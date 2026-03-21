ab0996d6690a6b882703c714fe70e4f7bbf8299a
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-21 21:40:29 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\scriptt.js

---
+++ operator_conditional_statement/scriptt.js
@@ -0,0 +1,77 @@
+// let age = 18;
+
+// if (age >= 18) {
+// console.log("you can vote");
+// }
+// if (age < 18){
+// console.log("you cannot vote");
+// }
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if (mode === "light"){
+//       color = "white";
+// }
+// console.log (color);
+
+
+// let mode = "light";
+// let color;
+// if (mode==="dark"){
+// color = "black";
+// }
+// else{
+//       color = "white";
+// }
+// console.log (color);
+
+
+// let age = 18;
+
+// if (age >= 18) {
+// console.log("you can vote");
+// }
+// else {
+// console.log("you cannot vote");
+// }
+
+// let num = 20;
+
+// if (num%2 === 0) {
+// console.log(num,"is even");
+// }
+// else {
+// console.log(num,"is odd");
+// }
+
+
+let mode ="silver";
+let color;
+
+if(mode === "dark"){
+   color="black";
+}
+else if (mode=== "blue"){
+    color = "blue";
+}
+else if (mode=== "pink"){
+    color = "pink";
+}
+else{
+    color = "white";
+} 
+
+console.log(color);
+
+
+
+
+
+
+
+
