d3850e7e8fe9580a594ea3f0bdfe9146185d0a7d
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-21 21:39:08 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\script.js

---
+++ operator_conditional_statement/script.js
@@ -0,0 +1,61 @@
+//arithematic operator -----
+// let a = 5;
+// let b = 2;
+// console.log("a = " + a ," & b = " + b);
+
+// console.log(" a+b = " , a+b);
+// console.log(" a-b = " , a-b);
+// console.log(" a*b = " , a*b);
+// console.log(" a/b = " , a/b);
+// console.log(" a%b = " , a%b);
+// console.log(" a**b = " , a**b); //5*5=25
+
+//UNARY OPERTOR -----
+// let a = 5;
+// let b = 2;
+// console.log("a = 5 " , a , "& b = 2" , b,);
+// console.log("--a" , --a);
+// console.log("a" , a);
+
+// Assignment operator -----
+
+//  let a = 5;
+//  let b = 2;
+//  a**= 4;
+// console.log("a = " , a ); //5*5*5*5 = 625
+
+//Comparision operator -----
+// let a = 5;
+//  let b = "5"; //first it convert it into number then compare
+//  console.log("a===b",a===b);
+
+
+//Logical operator (OR) -----
+// let a = 5;
+//  let b = 2;
+
+//  console.log("cond 1 && cond 2" , a < b || a===6);
+
+//  (AND) -----
+
+// let a = 5;
+// let b = 2;
+
+// let cond1= a>b;
+// let cond2= a===b;
+
+//  console.log("cond1 && cond2" ,cond1 && cond2 );
+
+ //(NOT)
+//  let a = 6;
+//  let b = 5;
+//  console.log("!(6 < 5)" , !(a===6));
+
+// TERNARY OPERATORS
+
+let age = 25;
+age > 18 ? console.log ("adult") : console.log ("not adult"); //simpler or compact if-else
+
+
+
+
