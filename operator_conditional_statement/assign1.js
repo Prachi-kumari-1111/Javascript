4712c0b6cefbadef717a5daa5c40e85379123517
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-24 12:04:51 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign1.js

---
+++ operator_conditional_statement/assign1.js
@@ -0,0 +1,40 @@
+// 🔹 PART 1: Operators
+// Q1.
+
+// 👉 2 variables banao:
+
+// a = 20
+// b = 4
+
+// 👉 print karo:
+
+// addition
+// subtraction
+// multiplication
+// division
+// modulus
+
+
+
+
+//------> let a=20;
+// let b=4;
+// let sum=a+b;
+// let sub=a-b;
+// let multiply=a*b;
+// let division=a/b;
+// let mod=a%b;
+// console.log("sum = ",sum );
+// console.log("sub = ",sub );
+// console.log("mul = ",multiply );
+// console.log("div = ",division );
+// console.log("mod = ",mod );
+
+
+let a = 20;
+let b = 4;
+console.log("sum = " ,a+b);
+console.log("sub = " ,a-b);
+console.log("mul = " ,a*b);
+console.log("div = " ,a/b);
+console.log("mod = " ,a%b);
