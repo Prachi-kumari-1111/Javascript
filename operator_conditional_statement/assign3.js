2d2125fd156849fe66b67d9a10675809ffa4d10a
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-24 12:08:22 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign3.js

---
+++ operator_conditional_statement/assign3.js
@@ -0,0 +1,16 @@
+// Q3.
+
+// 👉 Ek variable lo:
+
+// let a = 5;
+
+// 👉 print karo:
+
+// a++
+// a
+// ++a
+
+let a = 5;
+
+console.log("a++", a++ , a); //use first, then increase
+console.log("++a", ++a); //increase first, then use
