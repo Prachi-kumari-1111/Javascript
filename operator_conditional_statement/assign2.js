5219312aefd784a6ffed7206fd7f4d90db983b60
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-24 12:06:19 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign2.js

---
+++ operator_conditional_statement/assign2.js
@@ -0,0 +1,18 @@
+//QUESTION
+// Q2.
+
+// 👉 2 variables lo:
+
+// a = 10
+// b = "10"
+
+// 👉 print karo:
+
+// a == b
+// a === b
+
+
+let a = 10;
+let b ="10";
+console.log("a==b gives ", a==b);
+console.log("a===b gives ", a===b);
