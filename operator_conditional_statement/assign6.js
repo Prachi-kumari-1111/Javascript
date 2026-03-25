d6e79df3261ec21ffe5bbf2d61adf364640b4a52
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-25 23:35:09 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign6.js

---
+++ operator_conditional_statement/assign6.js
@@ -0,0 +1,18 @@
+// Q6.
+
+// 👉 Ek number lo
+// 👉 check karo:
+
+// positive
+// negative
+// zero
+
+let num = 76;
+if(num > 0){
+    console.log("Positive")
+}else if (num < 0){
+console.log("Negative");
+}
+else{
+    console.log("Zero");
+};
