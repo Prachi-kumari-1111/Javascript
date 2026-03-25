be854b18a3c7e641491f1de5394e13e173f7447f
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-25 21:03:20 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign4.js

---
+++ operator_conditional_statement/assign4.js
@@ -0,0 +1,15 @@
+// 🔹 PART 2: Conditions (if-else)
+// Q4.
+
+// 👉 Ek number lo
+// 👉 check karo:
+
+// even ya odd
+
+
+let a = 7;
+if(a%2 == 0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
