74a9e6c0f2b1f701d50e3b56470b5ed6263dca13
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-25 23:33:37 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign5.js

---
+++ operator_conditional_statement/assign5.js
@@ -0,0 +1,14 @@
+// Q5.
+
+// 👉 Ek age lo
+// 👉 check karo:
+
+// ≥18 → "Eligible for voting"
+// else → "Not eligible"
+
+let age = 18;
+if(age>=18){
+    console.log("Eligible for voting");
+}else{
+    console.log("Not Eligible");
+}
