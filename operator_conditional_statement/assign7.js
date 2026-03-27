f22f46520e75273c32c80aaffad42603dac7f038
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-27 20:24:32 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign7.js

---
+++ operator_conditional_statement/assign7.js
@@ -0,0 +1,18 @@
+// 🔹 PART 3: Logical Operators
+// Q7.
+
+// 👉 Variables lo:
+
+// age = 20
+// hasID = true
+
+// 👉 agar dono true → "Allowed"
+// 👉 warna → "Not allowed"
+
+let age = 21;
+let hasID = true;
+if (age>=20 && hasID ){
+    console.log("They are allowed")
+}else{
+    console.log("Not allowed");
+}
