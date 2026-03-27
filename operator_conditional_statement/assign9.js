826c0b00e1fe9a7b0a31535747ec881a085b44cc
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-27 20:25:50 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign9.js

---
+++ operator_conditional_statement/assign9.js
@@ -0,0 +1,31 @@
+// 🔹 PART 4: REAL TASK (🔥 must do)
+// Q9.
+
+// 👉 Ek variable lo: marks
+
+// 👉 condition lagao:
+
+// 90+ → "Grade A"
+// 70+ → "Grade B"
+// 50+ → "Grade C"
+// below → "Fail"
+
+// 👉 print:
+
+// Your grade is ___
+
+let marks = 92;
+if(marks>=90){
+    console.log("Grade A");
+}
+else if(marks >= 70){
+    console.log("Grade B");
+}
+else if (marks >= 50){
+    console.log("Grade C")
+}
+else{
+    console.log ("FAIL");
+}
+
+console.log("your marks is " , marks );
