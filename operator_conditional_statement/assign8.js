0793034dc8f890a0a3c05a377367ceef1f15e616
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-27 20:25:12 +0530

    e:\JavaScript\Javascript\operator_conditional_statement\assign8.js

---
+++ operator_conditional_statement/assign8.js
@@ -0,0 +1,19 @@
+// Q8.
+
+// 👉 Variables lo:
+
+// a = true
+// b = false
+
+// 👉 print karo:
+
+// a && b
+// a || b
+// !a
+
+
+let a = true;
+let b = false;
+console.log("AND CASE  ", a&&b );
+console.log("OR CASE " , a||b);
+console.log("NOT CASE ",!a );
