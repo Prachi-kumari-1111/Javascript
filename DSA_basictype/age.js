8b808f1886abd0af7d915232de9de2bb42d18905
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-28 22:33:17 +0530

    e:\JavaScript\Javascript\DSA_basictype\age.js

---
+++ DSA_basictype/age.js
@@ -0,0 +1,17 @@
+// 🟡 Q6 (real life logic)
+
+// 👉 Ek age lo
+// 👉 ek variable lo hasLicense = true/false
+
+// 👉 condition:
+
+// age ≥ 18 AND license hai → "Can Drive"
+// warna → "Cannot Drive"
+
+let age = 19;
+let hasLicense = false;
+if(age>=18 && hasLicense===true){
+    console.log("can drive");
+}else{
+    console.log("cannot drive")
+};
