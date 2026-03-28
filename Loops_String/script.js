ffc8425e32eb1b128ccc0c5a273461ef3a402e17
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-28 21:30:38 +0530

    e:\JavaScript\Javascript\Loops_String\script.js

---
+++ Loops_String/script.js
@@ -0,0 +1,37 @@
+// let str = "prachi";
+// console.log(str[5]);//P
+
+
+//Templete literals 
+// let obj = {
+//     item:"pen",
+//     price:10,
+// };
+// let output = `The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+
+// let specialString=`this is templete litrals ${1+2+3}`;
+// console.log(specialString);
+
+
+//Escape chracters:-
+// let str = "Apna\tcollege"; //12
+// console.log(str.length);
+
+//String method in js:---
+// 1.str.toUpperCase
+// let str = "Apna college";
+// str = str.toUpperCase();
+// console.log(str);
+
+
+// 2.str.toLpperCase
+// let str = "Apna college";
+// str = str.toLowerCase();
+// console.log(str);
+
+//3.str.trim()
+// let str = "      Apna  College js     ";//remove white spaces
+// console.log(str.trim());
+
