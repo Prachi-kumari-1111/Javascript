1a740634b4cb5796829fe29bb84d06a20f0a9f2f
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-28 22:34:48 +0530

    e:\JavaScript\Javascript\DSA_basictype\bignum.js

---
+++ DSA_basictype/bignum.js
@@ -0,0 +1,17 @@
+// 🟢 Q2.)
+// 👉 Do numbers lo
+// 👉 print karo:
+// 👉 bada number kaun sa hai
+// 👉 or agr dono equal hai to print kro "both are equal".
+
+
+let a = 3;
+let b = 8;
+if(a<b){
+console.log("Bignumber",b);
+}else if(b>a){
+    console.log(a)
+}
+else{
+console.log("both are equal")
+}
