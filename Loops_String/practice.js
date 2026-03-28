a2c9bd7b358e106c1d06584cc70874332e25007e
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-28 21:31:30 +0530

    e:\JavaScript\Javascript\Loops_String\practice.js

---
+++ Loops_String/practice.js
@@ -0,0 +1,10 @@
+// Let’s Practice
+
+// Qs1. Prompt the user to enter their full name. Generate a username for them based on the input. Start username with @, followed by their full name and ending with the fullname length.
+
+// eg: user name = “shradhakhapra”, username should be “@shradhakhapra13”
+
+let fullName = prompt ("Enter your name");
+let length = fullName.length;
+let username = "@"+ fullName + length; 
+console.log (username);
