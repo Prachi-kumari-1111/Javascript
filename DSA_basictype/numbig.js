daf367d7bfcf976368e1697ce994cd55724c94bb
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-29 21:15:16 +0530

    e:\JavaScript\Javascript\DSA_basictype\numbig.js

---
+++ DSA_basictype/numbig.js
@@ -0,0 +1,20 @@
+// 🟡 Q5
+
+// 👉 3 numbers lo
+// 👉 print karo:
+// 👉 sabse bada number
+
+let a =78;
+let b =56;
+let c =100;
+if(a<b){
+    console.log("bignumber",a);
+}else if (a>b){
+    console.log("bignumber",b);
+}else if (b<c){
+    console.log("bignumber",b);
+}else if(c<b){
+    console.log("bignumber",c);
+}else{
+    console.log("all numbers are equal")
+};
