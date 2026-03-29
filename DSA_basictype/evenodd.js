b0fd8f7067af85d9c7a13f34caab9116b6437667
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-29 21:12:00 +0530

    e:\JavaScript\Javascript\DSA_basictype\evenodd.js

---
+++ DSA_basictype/evenodd.js
@@ -0,0 +1,14 @@
+// Q1 (Easy Start)
+
+// 👉 Ek number lo
+// 👉 check karo:
+
+// even hai ya odd
+
+let a = 2;
+if(a%2 == 0){
+    console.log("Even number");
+}
+else{
+    console.log("odd number")
+};
