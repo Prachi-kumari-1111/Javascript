c6b9d0081204ed58b5bc6d330f1609ed0bab3bec
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-29 21:13:11 +0530

    e:\JavaScript\Javascript\DSA_basictype\fizz.js

---
+++ DSA_basictype/fizz.js
@@ -0,0 +1,21 @@
+// 🚀 Next (thoda spicy 😏):
+
+// 👉 Q4:
+// 👉 Ek number lo
+// 👉 check karo:
+
+// divisible by 3 AND 5 → "FizzBuzz"
+// only 3 → "Fizz"
+// only 5 → "Buzz"
+// else → number print
+
+let a = 22;
+if(a%3 === 0 && a%5 === 0){
+    console.log("FizzBuzz")
+}else if(a%3 === 0){
+    console.log("Fizz");
+}else if (a%5 === 0){
+    console.log("Buzz");
+}else{
+    console.log(a);
+}
