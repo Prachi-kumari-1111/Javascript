99849fe10b468ea0886d0da5c2c893a799fb1d9f
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-31 18:54:42 +0530

    e:\JavaScript\Javascript\DSA_basictype\divv.js

---
+++ DSA_basictype/divv.js
@@ -0,0 +1,12 @@
+// 👉 Ek number lo
+// 👉 check karo:
+
+// divisible by 3 AND 7 → "Yes"
+// warna → "No"
+
+let a = 21;
+if(a%3 === 0 && a%7 === 0){
+    console.log("YES divisible");
+}else{
+    console.log("Not Divisible");
+}
